a0cd3d2bc6d3d20a4b5803dda0a8ab3921894330
patliemoaloo <aloemo77@gmail.com>
2023-09-14 18:00:02 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);  
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number);
+        number--;
+    }
+}
